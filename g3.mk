2a20f4d00fec23d2f9ad71a6d64f27dd7a3effee
firebird11 <hbgassel@gmail.com>
2016-01-30 11:39:02 +0100

    g3: add initial quickcircle app
     *original commit by @codeworkx

--- g3.mk
+++ g3.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# QuickCircle Case App
+PRODUCT_PACKAGES += \
+    QuickCircle
+
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8974
